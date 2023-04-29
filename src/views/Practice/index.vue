c8be9c443631dc4bda74eb988047f2efe1975cb3
trongnguyendev <trongnguyendev@gmail.com>
2023-04-29 16:09:17 +0700

    .

--- src/views/Practice/index.vue
+++ src/views/Practice/index.vue
@@ -1,16 +1,10 @@
-<!-- <template>
+<template>
     <div>
         practice
-        <vue-speech @onTranscriptionEnd="onEnd" />
     </div>
 </template>
 
 <script lang="ts" setup>
 
-const re = SpeechRecognitionAlternative
-const onEnd = ({ lastSentence, transcription }) => {
-    console.log("aaaa");
-    console.log(lastSentence);
-    console.log(transcription);
-}
-</script> -->
+
+</script>
